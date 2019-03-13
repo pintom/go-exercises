ccba3b7337ef5c7a01ba67bca8bcb7ad4546b8e7
Tomas Tirlikas <tomastir@gmail.com>
2019-03-13 19:46:31 +0000

    exercise 1.2

--- chapter 1/ex1.1/main.go
+++ chapter 1/ex1.1/main.go
@@ -1,3 +1,7 @@
+/*
+Modify the echo program to also print os.Args[0], the name of the command that invoked it.
+*/
+
 package main
 
 import "os"
--- chapter 1/ex1.2/main.go
+++ chapter 1/ex1.2/main.go
@@ -1,3 +1,6 @@
+/*
+Modify the echo program to print the index and value of each of its arguments, one per line.
+*/
 package main
 
 import (
